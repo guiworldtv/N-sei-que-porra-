200c836eeddf7dcec37668127924fb0d46b0758c
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-09-01 15:33:32 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -18,7 +18,7 @@ def get_live_info(channel_id):
         url = urlMeta.get("content")
         if(url is None or url.find("/watch?v=") == -1):
             return None
-        titleMeta = soup.find("meta", property="og:image")
+        titleMeta = soup.find("meta", name="twitter:title")
         imageMeta = soup.find("meta", property="og:image")
         descriptionMeta = soup.find("meta", property="og:description")
         return {
