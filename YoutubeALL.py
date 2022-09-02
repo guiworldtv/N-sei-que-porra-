86930465ae6d6d886287f98b2f1aad5c7f84f3f5
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-09-02 02:56:49 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -1,5 +1,5 @@
 from yt_dlp import YoutubeDL
 
-URLS = ['https://www.youtube.com/watch?v=DTXDxJVhunw']
+URLS = ['https://www.youtube.com/watch?v=1eWMMCCkwQk']
 with YoutubeDL() as ydl:
     ydl.download(URLS)
