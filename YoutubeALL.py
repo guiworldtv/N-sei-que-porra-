43a3218672c07d13ad5b747133c8d37e000e9cff
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-09-01 22:24:14 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -1,40 +1,68 @@
-from __future__ import unicode_literals              # c)
- import youtube_dl
+from __future__ import unicode_literals
+import youtube_dl
 
 ydl_opts = {}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-     ydl.download(['https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s'])
-
- from __future__ import unicode_literals               # d)
- import youtube_dl
- ydl_opts = {}
- with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-     URL = ['https://www.youtube.com/watch?v=GIDoQsQyS0s', 'https://www.youtube.com/watch?v=Wv2rLZmbPMA']
-     ydl.download(URL)
-
- from __future__ import unicode_literals               # e)
- import youtube_dl
- ydl_opts = {}
- with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-     r = ydl.extract_info('https://www.youtube.com/watch?v=Wv2rLZmbPMA', download=False)
-     print(r)
-
- from __future__ import unicode_literals               # f) + h)
- import youtube_dl
- ydl_opts = {}
- with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-     URL = ['https://www.youtube.com/watch?v=Wv2rLZmbPMA', 'https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s', 'https://www.youtube.com/watch?v=GIDoQsQyS0s']
-     for i in URL:
-         ext = ydl.extract_info(i, download = False)
-     exted = [ext]
-     print(exted)
-
- import json
- var = exted
- with open('data1.json', 'w') as outfile:
-     json.dump(var, outfile)
+    ydl.download(['https://www.youtube.com/watch?v=Llw9Q6akRo4'])
+#tải video
 
 
- var = 'I love manga and anime'                        # g)
- with open('LOL.txt', 'a') as out:
-     out.write(var + '\n')
+import youtube_dl
+
+ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+
+with ydl:
+    result = ydl.extract_info(
+        'https://www.youtube.com/watch?v=Llw9Q6akRo4',
+        download=False
+    )
+print(result['uploader'],result['fps'])
+#tải information
+
+
+
+from __future__ import unicode_literals
+import youtube_dl
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=LSMDNL4n0kM'])
+#tải video convert mp3
+
+
+from youtube_dl import YoutubeDL
+
+options = {
+    'default_search': 'ytsearch5'
+}
+
+ydl = YoutubeDL(options)
+search_result = ydl.extract_info('that girl', False)
+print(search_result)
+# search video bằng tên
