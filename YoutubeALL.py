300130ae55aa4be4831e689ebf24b7cdbc91b69a
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-09-01 21:56:48 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -1,40 +1,40 @@
-# from __future__ import unicode_literals               c)
-# import youtube_dl
+from __future__ import unicode_literals              # c)
+ import youtube_dl
 
-# ydl_opts = {}
-# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#     ydl.download(['https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s'])
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+     ydl.download(['https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s'])
 
-# from __future__ import unicode_literals               # d)
-# import youtube_dl
-# ydl_opts = {}
-# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#     URL = ['https://www.youtube.com/watch?v=GIDoQsQyS0s', 'https://www.youtube.com/watch?v=Wv2rLZmbPMA']
-#     ydl.download(URL)
+ from __future__ import unicode_literals               # d)
+ import youtube_dl
+ ydl_opts = {}
+ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+     URL = ['https://www.youtube.com/watch?v=GIDoQsQyS0s', 'https://www.youtube.com/watch?v=Wv2rLZmbPMA']
+     ydl.download(URL)
 
-# from __future__ import unicode_literals               # e)
-# import youtube_dl
-# ydl_opts = {}
-# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#     r = ydl.extract_info('https://www.youtube.com/watch?v=Wv2rLZmbPMA', download=False)
-#     print(r)
+ from __future__ import unicode_literals               # e)
+ import youtube_dl
+ ydl_opts = {}
+ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+     r = ydl.extract_info('https://www.youtube.com/watch?v=Wv2rLZmbPMA', download=False)
+     print(r)
 
-# from __future__ import unicode_literals               # f) + h)
-# import youtube_dl
-# ydl_opts = {}
-# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#     URL = ['https://www.youtube.com/watch?v=Wv2rLZmbPMA', 'https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s', 'https://www.youtube.com/watch?v=GIDoQsQyS0s']
-#     for i in URL:
-#         ext = ydl.extract_info(i, download = False)
-#     exted = [ext]
-#     print(exted)
+ from __future__ import unicode_literals               # f) + h)
+ import youtube_dl
+ ydl_opts = {}
+ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+     URL = ['https://www.youtube.com/watch?v=Wv2rLZmbPMA', 'https://www.youtube.com/watch?v=n6BwAWiHcSg&t=182s', 'https://www.youtube.com/watch?v=GIDoQsQyS0s']
+     for i in URL:
+         ext = ydl.extract_info(i, download = False)
+     exted = [ext]
+     print(exted)
 
-# import json
-# var = exted
-# with open('data1.json', 'w') as outfile:
-#     json.dump(var, outfile)
+ import json
+ var = exted
+ with open('data1.json', 'w') as outfile:
+     json.dump(var, outfile)
 
 
-# var = 'I love manga and anime'                        # g)
-# with open('LOL.txt', 'a') as out:
-#     out.write(var + '\n')
+ var = 'I love manga and anime'                        # g)
+ with open('LOL.txt', 'a') as out:
+     out.write(var + '\n')
