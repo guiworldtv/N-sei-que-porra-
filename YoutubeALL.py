097f95ab881a2886b30759daf7555fe6ab27c148
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-09-10 04:05:19 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -1,110 +1,159 @@
-#! /usr/bin/python3
-
-
-from __future__ import unicode_literals
-import youtube_dl
+# -*- coding: utf-8 -*-
+#########################################################
+# python
+import os
+import sys
+import logging
+import traceback
+import json
+import re
+import urllib
 import requests
-import shutil
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-channel_no = 0
-m3u = None
-def get_live_info(channel_id):
-    try:
-        webpage = urlopen(f"{channel_id}").read()
-        soup = BeautifulSoup(webpage, 'html.parser')
-        urlMeta = soup.find("meta", property="og:url")
-        if urlMeta is None:
-            return None
-        url = urlMeta.get("content")
-        if(url is None or url.find("/watch?v=") == -1):
-            return None
-        titleMeta = soup.find("meta", property="og:title")
-        imageMeta = soup.find("meta", property="og:image")
-        descriptionMeta = soup.find("meta", property="og:description")
-        return {
-            "url": url,
-            "title": titleMeta.get("content"),
-            "image": imageMeta.get("content"),
-            "description": descriptionMeta.get("content")
-        }
+import threading
 
-    except Exception as e:
-                return None
+# third-party
+from lxml import html, etree
 
-banner = r'''
-#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"
-#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mudstein/XML/main/TIZENsiptv.xml"
-#EXTM3U x-tvg-url="https://raw.githubusercontent.com/K-vanc/Tempest-EPG-Generator/main/Siteconfigs/Argentina/%5BENC%5D%5BEX%5Delcuatro.com_0.channel.xml"
-#EXTM3U x-tvg-url="https://raw.githubusercontent.com/Nicolas0919/Guia-EPG/master/GuiaEPG.xml"
-'''
+# sjva 공용
+from framework import app
 
+# 패키지
+from .plugin import logger, package_name
+from .model import ModelSetting, ModelChannel
+from .source_base import SourceBase
+from support.base import d
 
+"""
+try:
+    #import youtube_dl
+    import yt_dlp
+except ImportError:
+    #os.system("{} install youtube_dl".format(app.config['config']['pip']))
+    os.system("{} install yt-dlp".format(app.config['config']['pip']))
+    #import youtube_dl    
+    import yt_dlp
+"""
+#########################################################
 
-def generate_youtube_tv():
-    global channel_no
-    ydl_opts = {
-        'format': 'best',
-    }
-    ydl = youtube_dl.YoutubeDL(ydl_opts)
+class YoutubedlItem:
+    ch_list = None
 
-    with open('YoutubeALL.txt') as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-            if line == "":
-                continue
-            channel = get_live_info(line)
-            if channel is None:
-                continue
-            try:
-                with ydl:
-                    result = ydl.extract_info(
-                        f"{line}/live",
-                        download=False  # We just want to extract the info
-                    )
+    def __init__(self, id, title, url):
+        self.id = id.strip()
+        self.title = title.strip()
+        self.url = url.strip()
+        YoutubedlItem.ch_list[id] = self
 
-                    if 'entries' in result:
-                        # Can be a playlist or a list of videos
-                        video = result['entries'][-1]
-                    else:
-                        # Just a video
-                        video = result
-                video_url = video['url']
+#python3 -m pip install -U yt-dlp
+class SourceYoutubedl(SourceBase):
+    channel_list = None
 
-                channel_no += 1
-                channel_name = f"{channel_no}-{line.split('/')[-1]}"
-                playlistInfo = f"#EXTINF:-1 tvg-chno=\"{channel_no}\" tvg-id=\"{line}\" tvg-name=\"{channel_name}\" tvg-logo=\"{channel.get('image')}\" group-title=\"ARGENTINA\",{channel.get('title')} - {channel.get('description')} - {channel_name}\n"
-                write_to_playlist(playlistInfo)
-                write_to_playlist(video_url)
-                write_to_playlist("\n")
-            except Exception as e:
-                print(e)
+    @staticmethod
+    def install():
+        try:
+            def func():
+                import system
+                commands = [
+                    ['msg', u'잠시만 기다려주세요.'],
+                    #['pip', 'install', '--upgrade', 'pip'],
+                    #['pip', 'install', '--upgrade', 'setuptools'],
+                    ['pip3', 'install', '--upgrade', 'yt-dlp'],
+                    ['msg', u'설치가 완료되었습니다.']
+                ]
+                system.SystemLogicCommand.start('설치', commands)
+            t = threading.Thread(target=func, args=())
+            t.setDaemon(True)
+            t.start()
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
 
 
+    @staticmethod
+    def is_installed():
+        try:
+            #import youtube_dl
+            import yt_dlp
+            return True
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+        return False
 
 
-def write_to_playlist(content):
-    global m3u    
-    m3u.write(content)
+    @classmethod
+    def get_channel_list(cls):
+        try:
+            tmp = ModelSetting.get('youtubedl_list')
+            YoutubedlItem.ch_list = {}
+            ret = []
+            for item in tmp.splitlines():
+                if item.strip() == '':
+                    continue
+                tmp2 = item.split('|')
+                if len(tmp2) != 3:
+                    continue
+                c = ModelChannel(cls.source_name, tmp2[0], tmp2[1], None, True)
+                YoutubedlItem(tmp2[0], tmp2[1], tmp2[2])
+                c.current = ''
+                ret.append(c)
+        except Exception as e:
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+        return ret
+
+    @classmethod
+    def get_url(cls, source_id, quality, mode):
+        try:
+            #logger.debug('source_id:%s, quality:%s, mode:%s', source_id, quality, mode)
+            #import youtube_dl
+            import yt_dlp
+            ydl_opts = {}
+            if ModelSetting.get_bool('youtubedl_use_proxy'):
+                ydl_opts['proxy'] = ModelSetting.get('youtubedl_proxy_url')
+            #ydl = youtube_dl.YoutubeDL(ydl_opts)
+            ydl = yt_dlp.YoutubeDL(ydl_opts)
+            target_url = YoutubedlItem.ch_list[source_id].url
+            """
+            logger.warning(target_url)
+            if target_url.startswith('YOUTUBE_'):
+                target_idx = int(target_url.split('_')[1]) - 1
+                live_home = 'https://www.youtube.com/playlist?list=PLU12uITxBEPGpEPrYAxJvNDP6Ugx2jmUx'
+                data = requests.get(live_home).text
+                root = html.fromstring(data)
+                tags = root.xpath('//td[@class="pl-video-title"]/a')
+                logger.debug(len(tags))
+                target_url = 'https://www.youtube.com' + tags[target_idx].attrib['href'].split('&')[0]
+                logger.info(target_url)
+            """
+            result = ydl.extract_info(target_url, download=False)
+            #logger.warning('Formats len : %s', len(result['formats']))
+            #logger.warning(d(result))
+            if 'formats' in result:
+                #formats = result['formats']
+                url = result['formats'][-1]['url']
+                #logger.error(url)
+            #logger.info(url)
+            if mode == 'web_play':
+                return 'return_after_read', url 
+            
+            return 'redirect', url
+        except Exception as e:
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+        return
+
+    @classmethod
+    def get_return_data(cls, source_id, url, mode):
+        try:
+            data = requests.get(url).text
+            return cls.change_redirect_data(data)
+        except Exception as e:
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+        return data
 
 
-def create_playlist():
-    global m3u
-    m3u = open("YoutubeALL.m3u8", "w")
-    m3u.write("#EXTM3U")
-    m3u.write("\n")
-
-
-def close_playlist():
-    global m3u
-    m3u.close()
-def generate_youtube_PlayList():
-    create_playlist()
-
-    m3u.write(banner)
-
-    generate_youtube_tv()
 
 
 
@@ -112,9 +161,10 @@ def generate_youtube_PlayList():
 
 
 
-    close_playlist()
 
 
 
-if __name__ == '__main__':
-    generate_youtube_PlayList()   
+
+
+
+
