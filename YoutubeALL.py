3c4bae995ab3dc8e0ca712d9d066039d36dfa5cb
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-09-10 02:49:42 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -5,7 +5,7 @@ URL = 'https://www.youtube.com/watch?v=BaW_jenozKc'
 
 # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
 ydl_opts = {
-    'title',
+    "title",
     }
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     info = ydl.extract_info(URL, download=False)
