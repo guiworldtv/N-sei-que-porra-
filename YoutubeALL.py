f7da0de183580ffbfdff7a2538fb47af072dd316
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-09-10 04:00:46 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -4,7 +4,9 @@ import yt_dlp
 URL = 'https://vimeo.com/748244075'
 
 # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
-ydl_opts = {}
+ydl_opts = {
+    'format_id': 'hls-akfire_interconnect_quic_sep-audio-medium-audio'
+    }
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     info = ydl.extract_info(URL, download=False)
 
